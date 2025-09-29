eca8adbd5160e23db5c6425d07924f3a38aeb0bb
MattiaToscano <mattia1997ct@gmail.com>
2025-09-29 12:00:18 +0200

    [UPD] Implementate rotte su web.php

--- ProgettoFinale/routes/web.php
+++ ProgettoFinale/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\FilmController;
+use App\Http\Controllers\GenreController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +17,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+    // Rotte per Film e Generi 
+    Route::resource('films', FilmController::class);
+    Route::resource('genres', GenreController::class);
 });
 
 require __DIR__.'/auth.php';
